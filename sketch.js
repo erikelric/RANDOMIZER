3542e158511ed8de7ac386afbc965606f6d67875
erikelric <60452164+erikelric@users.noreply.github.com>
2020-09-24 17:37:46 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+// types of jackets I like
+let jacket = [{
+  name: "coat",
+  color: "black"
+}, {
+  name: "blazer",
+  color: "grey"
+}, {
+  name: "hoodie",
+  color: "green"
+}, {
+  name: "parka",
+  color: "orange"
+}, {
+  name: "blouson",
+  color: "purple"
+}];
+
+let randomIndex;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(200);
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed(){
+
+  background(random(200, 255));
+  randomIndex = int(random(jacket.length));
+  text(jacket[randomIndex].name, 50, 50);
+  jacket.splice(randomIndex, 1);
+
 }
