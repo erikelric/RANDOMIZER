6d2cc6b542c2d848dd5f0bc3550d8a9ee73b682c
erikelric <60452164+erikelric@users.noreply.github.com>
2020-09-29 23:36:42 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,22 +17,44 @@ let jacket = [{
 }];
 
 let randomIndex;
+let animating = false;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
+  textSize(32);
+
+  text("click to randomize", 50, 50);
+
 
 }
 
 function draw() {
 
+  if(animating == true){
+    ellipse(random(width), random(height), random(50, 150))
+  }
 }
 
-function mousePressed(){
+function randomizer(){
+  animating = false;
 
-  background(random(200, 255));
-  randomIndex = int(random(jacket.length));
-  text(jacket[randomIndex].name, 50, 50);
-  jacket.splice(randomIndex, 1);
+  if (jacket[0]) {
+
+    background(random(200, 255));
+    randomIndex = int(random(jacket.length));
+    text(`${jacket[randomIndex].name}'s favorite color is ${jacket[randomIndex].color}`, 50, 50);
+    // text(jacket[randomIndex].name + "'s favorite color is" + jacket[randomIndex].color, 50, 50);
+    jacket.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("nothing left", 50, 50);
+    }
+
+}
+
+function mousePressed() {
+  animating = true;
+  setTimeout (randomizer, 2000);
 
 }
